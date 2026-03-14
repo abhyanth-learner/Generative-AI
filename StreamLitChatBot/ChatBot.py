7c6fea3f44300f42bb64f7e3690c5da69fa6f125
abhyanth-learner <abhyanth0809@gmail.com>
2026-03-14 11:54:33 +0530

    first commit

---
+++ StreamLitChatBot/ChatBot.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_groq import ChatGroq
+
+
+load_dotenv()
+
+st.set_page_config(
+    page_title="Chatbot",
+    page_icon="🤖",
+    layout="centered",
+)
+
+st.title("💬 Generative AI Chatbot")
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history=[]
+
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+llm=ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0.0,
+)
+
+user_prompt=st.chat_input("Ask Chatbot...")
+
+if user_prompt:
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role":"user","content":user_prompt})
+
+
+    response=llm.invoke(
+        input=[{"role":"system","content":"You ar a helpful chatbot.Give answer only related to cricket and football any other topic please respond with Sorry and some message of you own."},*st.session_state.chat_history])
+
+    assistant_response=response.content
+
+    st.session_state.chat_history.append({"role":"assistant","content":assistant_response})
+
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
